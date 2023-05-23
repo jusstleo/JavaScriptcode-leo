d8c126f43c6553b5f8767a14e1ea49d5ffc7c188
jusstleo <128613774+jusstleo@users.noreply.github.com>
2023-05-23 09:05:06 -0600

    operacion con numeros

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 console.log("hello world");
-alert("hoy le gano a juan pi");
+//alert("hoy le gano a juan pi");
+
+
+/*
+comentario multilinea
+*/
+
+//let nombre = prompt("cual es tu nombre?");
+//console.log(parseInt(nombre))
+
+//console.log (typeof(nombre))
+
+let numerouno = prompt ("ingresa un numero");
+let numerodos = prompt ("ingresa otro numero");
+
+let resultado = parseInt(numerouno) + parseInt(numerodos);
+let resultadoresta = parseInt(numerouno) - parseInt(numerodos);
+let resultadomultiplicacion = parseInt(numerouno) * parseInt(numerodos);
+let resultadodivision = parseInt(numerouno) / parseInt(numerodos);
+
+console.log(`el resultado de la suma es: ${resultado}`);
+console.log(`el resultado de la resta es: ${resultadoresta}`);
+console.log(`el resultado de la multiplicacion  es: ${resultadomultiplicacion}`);
+console.log(`el resultado de la division es: ${resultadodivision}`);
+
