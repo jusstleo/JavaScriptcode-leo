54e292d3c95f5dc491941e3ba87e6fd5490b6964
jusstleo <128613774+jusstleo@users.noreply.github.com>
2023-05-24 09:08:31 -0600

    condicionales

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ comentario multilinea
 
 //console.log (typeof(nombre))
 
+
+/*
 let numerouno = prompt ("ingresa un numero");
 let numerodos = prompt ("ingresa otro numero");
 
@@ -23,4 +25,22 @@ console.log(`el resultado de la suma es: ${resultado}`);
 console.log(`el resultado de la resta es: ${resultadoresta}`);
 console.log(`el resultado de la multiplicacion  es: ${resultadomultiplicacion}`);
 console.log(`el resultado de la division es: ${resultadodivision}`);
+*/
+
+/*
+let numerouno = prompt("ingresa un numero");
+let numerodos = prompt("ingresa otro numero");
+
+if (numerouno > numerodos) {
+    console.log(`el numero ${numerouno} es mayor que ${numerodos}`);
+} else if (numerodos > numerouno) {
+    console.log(`el numero ${numerodos} es mayor que ${numerouno}`);
+} else {
+    console.log(`el numero ${numerouno} es igual a ${numerodos}`);
+}
+*/
+
+let numerouno = prompt("ingresa un numero");
+let numerodos = prompt("ingresa otro numero");
+
 
